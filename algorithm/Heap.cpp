0591fbd34fd75af9aeb10e88a9c285388c746f10
jojo <jojo@DESKTOP-82DQHJ6.localdomain>
2020-02-06 00:04:45 +0800

    完善二叉堆

--- algorithm/Heap.cpp
+++ algorithm/Heap.cpp
@@ -33,6 +33,31 @@ class Heap{
 				tempCount=tempCount/2;
 			}
 		}
+		T remove(){
+			assert(count>0);
+			T item=data[1];
+			swap(data[1],data[count]);
+			count--;
+			int i=1;
+			while(2*i<=count){
+				//如果有右孩子并且左右孩子里大的那个比根节点更大 交换
+				if(2*i+1<=count){
+					int biggerIndex=data[i*2]>data[i*2+1]?2*i:i*2+1;
+					if(data[biggerIndex]>data[i]){
+						swap(data[i],data[2*i+1]);
+						i=biggerIndex;
+					}else//如果左右孩子都比根节点小 终点
+						break;
+				}else {//没有右孩子的时候只要和左孩子比较即可
+					if(data[i]<data[2*i]){
+						swap(data[i],data[2*i]);
+						i=2*i;
+					}else
+						break;
+				}
+			}
+			return item;
+		}
 		void show(){
 			for(int i=1;i<=count;i++)
 				cout<<data[i]<<"  ";
@@ -43,6 +68,9 @@ int main(){
 	for(int i=0;i<7;i++){
 		heap.insert(i+1);
 	}
+	heap.show();
+	cout<<"数量:"<<heap.size()<<endl;
+	heap.remove();
 	heap.show();
 	cout<<"数量:"<<heap.size()<<endl;
 	return 0;
